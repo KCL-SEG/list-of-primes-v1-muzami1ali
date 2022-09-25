8da19bb164024722fccc9fc52453465a2bd2e0e5
muzami1ali <muzamilalibosan@icloud.com>
2022-09-25 14:03:55 +0100

    Modify primes.py to return list of prime numbers

--- primes.py
+++ primes.py
@@ -2,5 +2,22 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def primes(number_of_primes):
-    list = []
-    return list
+    if number_of_primes>0:
+        list = []
+        count = 0
+        i = 2
+        x = 2 
+        while len(list)!=number_of_primes:
+            while x>0:
+                y = i%x
+                if y==0:
+                    count +=1
+                x -= 1
+            if count<3:
+                list.append(i)
+            count=0
+            i+=1
+            x = i
+        # for value in list:
+        #     print(f'{value}')
+        return list
